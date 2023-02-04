fabafffa37bf1164f2253a4bce27bb0c10acce13
Ekaterina <firegirl-83@mail.ru>
2023-02-04 09:41:22 +0300

    add task3_2

---
+++ task3_2/Program.cs
@@ -0,0 +1,28 @@
+﻿
+double[] arr = new double[10];
+
+void FillArray(double[] arr) {
+
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++) {
+
+        arr[i] = genRndDouble(-100,100,rnd);
+    
+    }
+}
+
+FillArray(arr);
+
+foreach (var item in arr)
+{
+    System.Console.WriteLine($"{item:f2}");
+
+}
+double genRndDouble (int a, int b, Random rnd)
+{
+
+    double rndRndDouble = a + rnd.NextDouble() * (b - a);
+
+    return rndRndDouble;
+
+}
